d3011be5b9a002e42fa635f36c5edff091e31d7e
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-10 11:23:23 +0900

    기본 기능, getStaticProps

--- next_test/src/pages/index.js
+++ next_test/src/pages/index.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+      <SearchInput placeholder="Filter by Name, Region or SubRegion" onChange={onInputChange} />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
